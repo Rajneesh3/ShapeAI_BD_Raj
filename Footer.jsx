5315697ad6b352af8eccaf1f67f665a00f377afe
Rajneesh3 <66130099+Rajneesh3@users.noreply.github.com>
2021-06-07 13:49:41 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  var currYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <p>Made by : Rajneesh Yadav</p>
+      <p>Copyright @ 2021</p>
+    </footer>
+  );
+}
+
+export default Footer;
